b6d62d921bc6c9b2b3d20f6a7e1b8b7f18038910
Ruud Verheijden <ruud.verheijden@gmail.com>
2018-09-08 11:21:47 +0200

    Let addon work from local fork for security reasons

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ docker run --rm --privileged \
     -v ~/.docker:/root/.docker \
     homeassistant/amd64-builder \
     --all \
-    -r https://github.com/korylprince/hassio-caddy.git \
+    -r https://github.com/ruudverheijden/hassio-caddy.git \
     -b master \
     -t caddy
